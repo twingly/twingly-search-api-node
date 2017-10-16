b022af5a183c2124bb72866b9a129ea3ae520d55
Robin Wallin <robin@twingly.com>
2017-10-16 12:06:05 +0200

    Do not include TZ information in queries
    and re-record VCR cassettes.

--- test/cassettes/search_without_valid_api_key.js
+++ test/cassettes/search_without_valid_api_key.js
@@ -4,7 +4,7 @@ var refs = [];
 refs[0] = nock('http://api.twingly.com:443')
   .get('/blog/search/api/v3/search?apikey=wrong&q=something')
   .reply(401, "<?xml version=\"1.0\" encoding=\"utf-8\"?><error code=\"40101\"><message>Unauthorized</message></error>", { server: 'nginx',
-  date: 'Fri, 05 May 2017 11:51:05 GMT',
+  date: 'Mon, 16 Oct 2017 10:03:50 GMT',
   'content-type': 'application/xml; charset=utf-8',
   'content-length': '97',
   connection: 'close',
