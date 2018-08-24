431c278a1ddb1ed2ee28924bf992aa851e80a70d
Mattias Roback <mattias.roback@twingly.com>
2018-08-24 10:29:52 +0200

    Add sepia (nock-vcr replacement)
    No fixtures has been added yet. I'll see if the API key handling
    in the specs can be improved before recording anything.
    
    Related to #17

--- test/support/setup.js
+++ test/support/setup.js
@@ -1,6 +1,13 @@
 var Helpers = require('./helpers').Helpers;
 var testHelper = new Helpers();
 
+// Do not record new fixtures if one already exist
+process.env['VCR_MODE'] = 'cache';
+
+var path = require('path');
+var sepiaVcr = require('sepia');
+sepiaVcr.fixtureDir(path.join(process.cwd(), 'test', 'cassettes'));
+
 // Fake API key for tests
 process.env['TWINGLY_SEARCH_KEY'] = testHelper.randomValueHex(16);
 
